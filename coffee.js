6b2b9245c0b0d68a71c0f3cdb98cbf8996ec2c0a
niyonkurutresor <niyonkurutresor17@gmail.com>
2022-10-24 18:08:39 +0200

    update

--- coffee.js
+++ coffee.js
@@ -5,6 +5,7 @@ let bagle = document.querySelector(".fa-bars");
 let asideMenu = document.querySelector(".aside_navigation");
 let switchMode = document.querySelector(".fa-adjust");
 let preloader = document.querySelector(".preloader");
+let picture = document.querySelectorAll(".divp")
 
 
 bagle.addEventListener("click", ()=>{
@@ -19,4 +20,9 @@ switchMode.addEventListener("click",()=>{
 
 window.addEventListener("load",()=>{
     preloader.style.display = "none"
+})
+
+for(let i=0;i< picture.length; i++)
+picture[i].addEventListener("click",()=>{
+    picture[i].classList.add("try");
 })
